7c2116502706f8c6f02341a0932c3669c4d31850
Md Mosiur Rahman <mosiur.404.rahman@gmail.com>
2024-08-10 05:00:28 +1000

    added skeleton component

---
+++ angular-frontend/src/app/todo-item-skeleton/todo-item-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-todo-item-skeleton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item-skeleton.component.html',
+  styleUrl: './todo-item-skeleton.component.scss'
+})
+export class TodoItemSkeletonComponent {
+
+}
